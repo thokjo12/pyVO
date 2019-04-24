4b280b067ae6d46c1946d6deef5e7ca0504ffb91
Chrizzsh <chrissha@stud.ntnu.no>
2019-04-24 17:29:48 +0200

    Implemented project points

--- pointProjection.py
+++ pointProjection.py
@@ -8,7 +8,6 @@ fy = 521.0
 cx = 325.1
 cy = 249.7
 
-
 def project_points(ids: np.ndarray, points: np.ndarray, depth_img: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """
     Projects the 2D points to 3D using the depth image and the camera instrinsic parameters.
@@ -18,12 +17,9 @@ def project_points(ids: np.ndarray, points: np.ndarray, depth_img: np.ndarray) -
     :return: A tuple containing a N vector and a 3xN vector of all the points that where successfully projected.
     """
     points = points.astype('int')
-    points_depth = depth_img[points[0], points[1]]
-    points_depth = np.expand_dims(points_depth, axis=0)
-    projected_points = np.append(points, points_depth, axis=0)
-
-    K = np.array([fx, 0., cx,
-                  0., fy, cy,
-                  0., 0., 1])
+    Z = depth_img[points[0], points[1]] / 5000
+    X = (points[0] - cx) * Z / fx
+    Y = (points[1] - cy) * Z / fy
+    projected_points = np.block([[X], [Y], [Z]])
 
     return (ids, projected_points)
--- visualodometry.py
+++ visualodometry.py
@@ -61,9 +61,5 @@ while dl.has_next():
     points_and_response = harris_corners(grey_img)
     tracker.add_new_corners(grey_img, points_and_response)
 
-    # Find transformation of the new frame
-    ## I will push this code to the repo a bit later, as there is still some smaller issues to sort out with it
-
-
 
 cv2.destroyAllWindows()
